ea4eff9aa78c9298ab8bcf1a263397e1eb4cc3b2
Emili Kaiman <emilikaiman@gmail.com>
2021-12-03 13:46:06 -0700

    begin order testing

--- src/order.js
+++ src/order.js
@@ -1,7 +1,39 @@
+function takeOrder(xx, array){
+  if ( array.length < 3){
+  array.push(xx)
+}
+  return array
+}
+
+function refundOrder(orderNum, listspot){
+  for ( i = 0; i < listspot.length; i++){
+    if (orderNum === listspot[i].orderNumber){
+      listspot.splice([i], 1);
+    }
+  }
+return listspot;
+}
+
+
+function listItems(order){
+  var orders = [];
+  for ( i = 0; i < order.length; i++){
+    orders.push(order[i].item)
+
+  }
+  return orders.join(', ');
+}
+
+// function listItems(order){
+//   for ( i = 0; i < order.length; i++){
+//   return order[i].item
+//   }
+// }
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+takeOrder,
+refundOrder,
+listItems,
+// searchOrder
 }

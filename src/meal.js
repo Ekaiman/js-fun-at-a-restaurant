0edc58d291a6b3e47a128e49c4608f78f3f54ab1
Emili Kaiman <emilikaiman@gmail.com>
2021-12-02 17:25:26 -0700

    Add meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,51 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+return `Delicious ${name}`;
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+      name: name,
+      price: price,
+      type: type,
+    }
+  return menuItem
+}
+
+function addIngredients(item1, ingredients){
+  if (!ingredients.includes(item1)){
+    ingredients.push(item1);
+  }
+
+  return ingredients;
+}
+
+
+function formatPrice(x){
+  return `$${x}`
+}
+
+
+function decreasePrice(xxxx) {
+  return (xxxx - (xxxx * .1))
+}
+
+
+function createRecipe(title, ingredients, menuItemType){
+  recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+
+return recipe 
+
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
-
-
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
+};

1912463b9ab42ed516a6afb4369b47cc24c22ad0
Emili Kaiman <emilikaiman@gmail.com>
2021-12-05 17:45:24 -0700

    add restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,63 @@
+var restaurant = {};
+
+function createRestaurant(name){
+  restaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  }
+  return restaurant
+}
+
+
+// function addMenuItem(name, newMenu){
+//   restaurant.menus.lunch.push(newMenu)
+// }
+
+
+
+  function addMenuItem(restaurantObject, newMenu){
+
+      if (newMenu.type == 'breakfast' && restaurantObject.menus.breakfast.includes(newMenu) == false){
+        restaurant.menus.breakfast.push(newMenu);
+      }
+
+      if (newMenu.type == 'lunch' && restaurantObject.menus.lunch.includes(newMenu) == false){
+        restaurant.menus.lunch.push(newMenu);
+      }
+
+      if (newMenu.type == 'dinner' && restaurantObject.menus.dinner.includes(newMenu) == false){
+        restaurant.menus.dinner.push(newMenu);
+      }
+}
+
+// function addMenuItem(restaurantObject, newMenu){
+//     if (newMenu.type == 'breakfast'){
+//       if ()
+//       for (i = 0; i < restaurant.menu.breakfast.length; i++){
+//       restaurant.menus.breakfast.push(newMenu);
+//     }
+//   }
+//
+//     if (newMenu.type == 'lunch'){
+//       restaurant.menus.lunch.push(newMenu);
+//     }
+//
+//     if (newMenu.type == 'dinner'){
+//       restaurant.menus.dinner.push(newMenu);
+//     }
+// }
+
+function removeMenuItem(discard){
+  restaurant.menus.breakfast.pop(discard)
+}
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+createRestaurant,
+addMenuItem,
+removeMenuItem
 }
